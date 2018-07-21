1d8d1a0754cb870c7943ab4b34bb154a235c0819
tomming233 <tomming233@gmail.com>
2018-07-21 11:09:19 +0800

    practice

---
+++ src/practice/cp1_4/TwoSumFast.java
@@ -0,0 +1,28 @@
+package practice.cp1_4;
+
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.BinarySearch;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class TwoSumFast {
+	public static int count(int[] a) {
+		Arrays.sort(a);
+		int N = a.length;
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			if (BinarySearch.rank(-a[i], a) > i) {
+				cnt++;
+			}
+		}
+		return cnt;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = In.readInts(args[0]);
+		StdOut.println(count(a));
+	}
+
+}
