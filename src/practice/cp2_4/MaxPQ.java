4d9cdb1769b80d25f17b1c04ba795653ead39617
tomming233 <tomming233@gmail.com>
2018-08-11 00:05:57 +0800

    maxPQ

--- src/practice/cp2_4/MaxPQ.java
+++ src/practice/cp2_4/MaxPQ.java
@@ -6,6 +6,19 @@ public class MaxPQ<Key extends Comparable<Key>> {
 	private Key[] pq;
 	private int N = 0;
 	
+	public MaxPQ(int maxN)
+	{
+		pq = (Key[]) new Comparable[maxN + 1];
+	}
+	
+	public boolean isEmpty() {
+		return N == 0;
+	}
+	
+	public int size() {
+		return N;
+	}
+	
 	private boolean less(int i, int j)
 	{
 		return pq[i].compareTo(pq[j]) < 0;
@@ -25,5 +38,30 @@ public class MaxPQ<Key extends Comparable<Key>> {
 			exch(k/2, k);
 			k = k/2;
 		}
+	}
+	
+	private void sink(int k)
+	{
+		while (2*k <= N)
+		{
+			int j = 2*k;
+			if (j < N && less(j, j+1)) j++;
+			if (!less(k, j)) break;
+			exch(k, j);
+			k = j;
+		}
+	}
+	
+	public void insert(Key v) {
+		pq[++N] = v;
+		swim(N);
+	}
+	
+	public Key delMax() {
+		Key max = pq[1];
+		exch(1, N--);
+		pq[N+1] = null;
+		sink(1);
+		return max;
 	}
 }
