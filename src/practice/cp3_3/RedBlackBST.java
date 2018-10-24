e040a0400c98d118bdbe0b720c2ec5985b2cf1ba
tomming233 <tomming233@gmail.com>
2018-10-24 23:57:20 +0800

    learning

--- src/practice/cp3_3/RedBlackBST.java
+++ src/practice/cp3_3/RedBlackBST.java
@@ -8,11 +8,11 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
 	private Node root;
 	
 	private class Node {
-		Key key;
-		Value val;
-		Node left, right;
-		int N;
-		boolean color;
+		private Key key;
+		private Value val;
+		private Node left, right;
+		private int N;
+		private boolean color;
 		
 		Node(Key key, Value val, int N, boolean color) {
 			this.key = key;
@@ -25,5 +25,70 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
 	private boolean isRed(Node x) {
 		if (x == null) return false;
 		return x.color == RED;	
+	}
+	
+	private int size(Node x) {
+		if (x == null) return 0;
+		return x.N;
+	}
+	
+	public int size() {
+		return size(root);
+	}
+	
+	public boolean isEmpty() {
+		return root == null;
+	}
+	
+	public void put(Key key, Value val) {
+		root = put(root, key, val);
+		root.color = BLACK;
+	}
+	
+	private Node put(Node h, Key key, Value val) {
+		if (h== null)
+			return new Node(key, val, 1, RED);
+		
+		int cmp = key.compareTo(h.key);
+		if      (cmp < 0) h.left = put(h.left, key, val);
+		else if (cmp > 0) h.right = put(h.right, key, val);
+		else h.val = val;
+		
+		if (isRed(h.right) && !isRed(h.left))      h = rotateLeft(h);
+		if (isRed(h.left) && isRed(h.left.left))   h= rotateRight(h);
+		if (isRed(h.right) && isRed(h.left))       flipColors(h);
+		
+		h.N = size(h.left) + size(h.right);
+		return h;
+	}
+	
+	   /*************************************************************************
+	    *  red-black tree helper functions
+	    *************************************************************************/
+	
+	private Node rotateRight(Node h) {
+		Node x = h.right;
+		h.right = x.left;
+		x.left = h;
+		x.color = h.color;
+		x.N = h.N;
+		h.N = 1 + size(h.left) + size(h.right);
+		return x;
+	}
+	
+	private Node rotateLeft(Node h) {
+		Node x = h.left;
+		h.left = x.right;
+		x.right = h;
+		x.color = h.color;
+		x.N = h.N;
+		h.N = 1 + size(h.left) + size(h.right);
+		return h;
+	}
+	
+	private void flipColors(Node h) {
+		h.color = RED;
+		h.left.color = BLACK;
+		h.right.color = BLACK;
 	}
 }
