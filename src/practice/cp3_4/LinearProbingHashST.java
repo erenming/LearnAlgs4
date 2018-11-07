8a98f7ee68f9d4cfd5f6ab2d33231e2f38432e98
tomming233 <tomming233@gmail.com>
2018-11-07 23:55:57 +0800

    learning

--- src/practice/cp3_4/LinearProbingHashST.java
+++ src/practice/cp3_4/LinearProbingHashST.java
@@ -1,9 +1,13 @@
 package practice.cp3_4;
 
+import java.awt.Checkbox;
+
+import edu.princeton.cs.algs4.Queue;
+
 public class LinearProbingHashST<Key, Value> {
     private static final int INIT_CAPACITY = 4;
 
-    private int N;
+    private int N;                 // number of key-value pairs in the symbol table
     private int M;
     private Key[] keys;
     private Value[] vals;
@@ -27,8 +31,16 @@ public class LinearProbingHashST<Key, Value> {
 		return N;
 	}
     
-    private void resize() {
-		
+    private void resize(int capacity) {
+		LinearProbingHashST<Key, Value> temp = new LinearProbingHashST<Key, Value>(capacity);
+		for (int i = 0; i < M; i++) {
+			if (keys[i] != null) {
+				temp.put(keys[i], vals[i]);
+			}
+		}
+		keys = temp.keys;
+		vals = temp.vals;
+		M = temp.M;
 	}
     
     public void put(Key key, Value val) {
@@ -53,4 +65,68 @@ public class LinearProbingHashST<Key, Value> {
 		}
 		return null;
 	}
+    
+    public boolean contains(Key key) {
+		return get(key) != null;
+	}
+    
+    public void delete(Key key) {
+		if (!contains(key)) return;
+		
+		// find position of key
+		int i = hash(key);
+		while (!key.equals(keys[i])) {
+			i = (i + 1) % M;
+		}
+		
+		// delete key and associated vale
+		keys[i] = null;
+		vals[i] = null;
+		
+		// rehash all keys in same cluster
+		i = (i + 1) % M;
+		while (keys[i] != null) {
+            // delete keys[i] an vals[i] and reinsert
+			Key    keyToRefresh = keys[i];
+			Value  valToRefresh = vals[i];
+			keys[i] = null;
+			vals[i] = null;
+			N--;
+			put(keyToRefresh, valToRefresh);
+			i = (i + 1) % M;
+		}
+		N--;
+		
+		if (N > 0 && N <= M/8) resize(M/2);
+		
+		assert check();
+	}
+    
+    public Iterable<Key> keys() {
+    	Queue<Key> queue = new Queue<Key>();
+    	for (int i = 0; i < M; i++) {
+    		if (keys[i] != null) queue.enqueue(keys[i]);
+    	}
+    	return queue;
+    }
+    
+    // integrity check - don't check after each put() because
+    // integrity not maintained during a delete()
+    private boolean check() {
+    	// check that hash is at most %50 full
+    	if (M < 2*N) {
+    		System.err.println("Hash table size M = " + M + "; array size N = " + N);
+    		return false;
+    	}
+    	
+    	// check that each key in table can by found by key
+    	for (int i = 0; i < M; i++) {
+    		if (keys[i] == null) continue;
+    		else if (get(keys[i]) != vals[i]) {
+                System.err.println("get[" + keys[i] + "] = " + get(keys[i]) + "; vals[i] = " + vals[i]);
+                return false;
+    		}
+    	}
+    	return true;
+    }
 }
--- src/practice/cp3_4/SeparateChainingHashST.java
+++ src/practice/cp3_4/SeparateChainingHashST.java
@@ -1,5 +1,6 @@
 package practice.cp3_4;
 
+import edu.princeton.cs.algs4.Queue;
 import practice.cp3_1.SequentialSearchST;
 
 public class SeparateChainingHashST<Key, Value> {
@@ -22,9 +23,33 @@ public class SeparateChainingHashST<Key, Value> {
 		}
 	}
 	
+	private void resize(int chains) {
+		SeparateChainingHashST<Key, Value> temp = new SeparateChainingHashST<Key, Value>(chains);
+		for (int i = 0; i < M; i++) {
+			for (Key key : st[i].keys()) {
+				temp.put(key, st[i].get(key));
+			}
+		}
+		this.M = temp.M;
+		this.N = temp.N;
+		this.st = temp.st;
+	}
+	
 	// hash value between 0 and M -1
 	private int hash(Key key) {
 		return (key.hashCode() & 0x7fffffff) % M;
+	}
+	
+	public int size() {
+		return N;
+	}
+	
+	public boolean isEmpty() {
+		return size() == 0;
+	}
+	
+	public boolean contains(Key key) {
+		return get(key) != null;
 	}
 	
 	// return value associated with key, null if no such key
@@ -33,7 +58,28 @@ public class SeparateChainingHashST<Key, Value> {
 		return st[i].get(key);
 	}
 	
-	public void put(Key key, Value val) {
-		st[hash(key)].put(key, val);
+	public void delete(Key key) {
+		int i = hash(key);
+		if (st[i].contains(key)) N--;
+		st[i].delete(key);
+        if (M > INIT_CAPACITY && N <= 2*M) resize(M/2);
 	}
+	
+	public void put(Key key, Value val) {
+        if (val == null) { delete(key); return; }
+		
+		st[hash(key)].put(key, val);
+        // double table size if average length of list >= 10
+        if (N >= 10*M) resize(2*M);
+	}
+	
+    // return keys in symbol table as an Iterable
+    public Iterable<Key> keys() {
+        Queue<Key> queue = new Queue<Key>();
+        for (int i = 0; i < M; i++) {
+            for (Key key : st[i].keys())
+                queue.enqueue(key);
+        }
+        return queue;
+    } 
 }
