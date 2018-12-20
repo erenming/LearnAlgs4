960489d4b7a752e5ce95b80efa464f6e474ebe98
tomming233 <tomming233@gmail.com>
2018-12-21 00:06:44 +0800

    learning

---
+++ src/practice/cp4_4/EdgeWeightedDigraph.java
@@ -0,0 +1,79 @@
+package practice.cp4_4;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class EdgeWeightedDigraph {
+    private final int V;
+    private int E;
+    private Bag<DirectedEdge>[] adj;
+
+    public EdgeWeightedDigraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<DirectedEdge>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<DirectedEdge>();
+        }
+    }
+
+    public EdgeWeightedDigraph(In in) {
+        this(in.readInt());
+        int E = in.readInt();
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            double weight = in.readDouble();
+            DirectedEdge e = new DirectedEdge(v, w, weight);
+            addEdge(e);
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(DirectedEdge e) {
+        adj[e.from()].add(e);
+        E++;
+    }
+
+    public Iterable<DirectedEdge> adj(int v) {
+        return adj[v];
+    }
+
+    public Iterable<DirectedEdge> edges() {
+        Bag<DirectedEdge> bag = new Bag<DirectedEdge>();
+        for (int v = 0; v < V; v++)
+            for (DirectedEdge e : adj[v])
+                bag.add(e);
+        return bag;
+    }
+
+    public String toString() {
+        String NEWLINE = System.getProperty("line.separator");
+        StringBuilder s = new StringBuilder();
+        s.append(V + " " + E + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(v + ": ");
+            for (DirectedEdge e : adj[v]) {
+                s.append(e + "  ");
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
+
+    // test
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        EdgeWeightedDigraph G = new EdgeWeightedDigraph(in);
+        StdOut.println(G);
+    }
+}
